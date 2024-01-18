064a85c0bbe6a520623bfa9fa95617b7e7e8d2c2
sangka1 <65866640+sangka1@users.noreply.github.com>
2024-01-18 23:37:56 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,1 @@
 AOS.init();
-       
-   class Main extends React.Component {
-         render() {
-           const img = "https://qu.ax/SEeO.jpeg"
-           return (
-             <div className = "main">
-               <h1 id = "main"> Selamat Datang </h1>
-             </div>
-           )
-      }
-   }
-   
-ReactDOM.render(<Main/>, document.getElementById("app"))
